9b452807040a8b1dc5ddb499d8f9db571cc6baca
sujancse5bu <60531496+sujancse5bu@users.noreply.github.com>
2021-02-05 09:30:38 +0600

    admin features created

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -11,7 +11,7 @@ const getProducts = asyncHandler(async (req, res) => {
 })
 
 // @desc    Fetch single Produts
-// @route   GET /api/products:id
+// @route   GET /api/products/:id
 // @access  Public
 const getProductById = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id)
@@ -23,4 +23,76 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 })
 
-export { getProducts, getProductById }
+// @desc    DELETE a single product
+// @route   GET /api/products/:id
+// @access  private/Admin
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id)
+  if (product) {
+    await product.remove()
+    res.json({ message: 'Product Removed' })
+  } else {
+    res.status(404)
+    throw new Error('Product not found.')
+  }
+})
+
+// @desc    create a single product
+// @route   POST /api/products
+// @access  private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'sample name',
+    price: 0,
+    user: req.user._id,
+    image: '/images/sample.png',
+    brand: 'Sample brand',
+    category: 'Sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample description',
+  })
+
+  const createdProduct = await product.save()
+  res.status(201).json(createdProduct)
+})
+
+// @desc    update a single product
+// @route   put /api/products/:id
+// @access  private/Admin
+const updateProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    iamge,
+    brand,
+    category,
+    countInStock,
+  } = req.body
+
+  const product = await Product.findById(req.params.id)
+  if (product) {
+    product.name = name
+    product.price = price
+    product.description = description
+    product.iamge = iamge
+    product.brand = brand
+    product.category = category
+    product.countInStock = countInStock
+
+    const updatedProduct = await product.save()
+    res.json(updatedProduct)
+  } else {
+    req.status(404)
+    throw new Error('Product not found')
+  }
+})
+
+export {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+}
